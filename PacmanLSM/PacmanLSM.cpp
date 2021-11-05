3b90a238c1bc2a34e016fe178644cd4c00e1778e
lulabai <catasamo@gmail.com>
2021-11-05 16:02:22 +0100

    LOGICA Y CHOQUE CONTRA PAREDES

--- PacmanLSM/PacmanLSM.cpp
+++ PacmanLSM/PacmanLSM.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
 using namespace std;
 
-//podemos usar MACROS para buscar y reemplazar:
-#define CONSOLE_HEIGHT 29 //eje y
-#define CONSOLE_WIDTH 119 // eje x
-#define BORDER_X 3
-#define BORDER_Y 5
 
-char ConsoleScreen[CONSOLE_HEIGHT][CONSOLE_WIDTH];
+#define CONSOLE_HEIGHT 29 //eje y filas
+#define CONSOLE_WIDTH 119 // eje x columnas
+#define BORDER_X 1
+#define BORDER_Y 2
+
+int main();
+void crearMapa();
+void imprimirMapa();
+void inputs();
+void logica();
+
+
+enum MAP_TILES { EMPTY = ' ', WALL = '#', POINT = '.' };
+enum USER_INPUTS { NONE, UP, DOWN, RIGHT, LEFT, QUIT };
+
+
+MAP_TILES ConsoleScreen[CONSOLE_HEIGHT][CONSOLE_WIDTH];
 char personaje = 'O';
-int personaje_posicion_x = 100;
-int personaje_posicion_y = 20;
+int personaje_posicion_x = 10;
+int personaje_posicion_y = 5;
+USER_INPUTS input = USER_INPUTS::NONE;
 bool run = true;
+
+
+int main()
+{
+	crearMapa();
+	imprimirMapa();
+	while (run) {
+		inputs();
+		logica();
+		imprimirMapa();
+	}
+}
 
 
 void imprimirMapa()
 {
+	system("CLS");
 	for (int y = 0; y < CONSOLE_HEIGHT; y++)
 	{
 		for (int x = 0; x < CONSOLE_WIDTH; x++)
@@ -25,11 +50,10 @@ void imprimirMapa()
 				cout << personaje;
 			}
 			else {
-				cout << ConsoleScreen[y][x];
+				cout << (char)ConsoleScreen[y][x];
 			}
 		}
 		cout << endl;
-
 	}
 }
 
@@ -46,50 +70,81 @@ void crearMapa()
 			if (y < BORDER_X || y >= CONSOLE_HEIGHT - BORDER_X || x < BORDER_Y || x >= CONSOLE_WIDTH - BORDER_Y) {
 				// OTRA FORMA: if (y  == 0 || y == CONSOLE_HEIGHT - 1 || x == 0 || x >= CONSOLE_WIDTH - 1) {} --> aquí hace un marco de uno por cada lado
 
-				ConsoleScreen[y][x] = '#';
+				ConsoleScreen[y][x] = MAP_TILES::WALL;
 			}
 			else {
-				//Acceder a los elementos del array
-				ConsoleScreen[y][x] = '-'; //y filas, x columnas
+				ConsoleScreen[y][x] = MAP_TILES::EMPTY;
 			}
 		}
 	}
 }
 
+
 void inputs()
 {
-	char input;
-	cin >> input;
-	switch (input)
+	char input_raw;
+	cin >> input_raw;
+	switch (input_raw)
 	{
 	case 'W':
 	case 'w':
-		personaje_posicion_y++; // es lo mismo que: personaje_posicion_y=personaje_posicion_y+1; o es lo mismo que: personaje_posicion_y += 1;
+		input = USER_INPUTS::UP;
 		break;
 	case 'A':
 	case 'a':
-		personaje_posicion_x--;
+		input = USER_INPUTS::LEFT;
 		break;
 	case 'S':
 	case 's':
-		personaje_posicion_y--;
+		input = USER_INPUTS::DOWN;
 		break;
 	case 'D':
 	case 'd':
-		personaje_posicion_x++;
+		input = USER_INPUTS::RIGHT;
+		break;
+	case 'Q':
+	case 'q':
+		input = USER_INPUTS::QUIT;
 		break;
 	default:
+		input = USER_INPUTS::NONE;
 		break;
 	}
 
 }
 
-int main()
+void logica()
 {
-	crearMapa();
-	imprimirMapa();
-	while (run) {
-		inputs();
-		imprimirMapa();
+	int personaje_y_new = personaje_posicion_y;
+	int personaje_x_new = personaje_posicion_x;
+
+
+	switch (input)
+	{
+	case NONE:
+		break;
+	case UP:
+		personaje_y_new--;
+		break;
+	case DOWN:
+		personaje_y_new++;
+		break;
+	case RIGHT:
+		personaje_x_new++;
+		break;
+	case LEFT:
+		personaje_x_new--;
+		break;
+	case QUIT:
+		run = false;
+		break;
+	default:
+		break;
 	}
+	if (ConsoleScreen[personaje_y_new][personaje_x_new] == MAP_TILES::WALL) {
+		personaje_y_new = personaje_posicion_y;
+		personaje_x_new = personaje_posicion_x;
+	}
+	personaje_posicion_y = personaje_y_new;
+	personaje_posicion_x = personaje_x_new;
 }
