dc4910812b1c9362d0e2c73dba17aa20f42c97b6
lulabai <catasamo@gmail.com>
2021-11-09 16:17:53 +0100

    Crear funciones para crear mapa y dibujar mapa

--- PacmanLSM/PacmanLSM.cpp
+++ PacmanLSM/PacmanLSM.cpp
@@ -10,31 +10,21 @@ using namespace std;
 int main();
 void crearMapa();
 void imprimirMapa();
-void inputs();
-void logica();
 
 
 enum MAP_TILES { EMPTY = ' ', WALL = '#', POINT = '.' };
-enum USER_INPUTS { NONE, UP, DOWN, RIGHT, LEFT, QUIT };
 
 
 MAP_TILES ConsoleScreen[CONSOLE_HEIGHT][CONSOLE_WIDTH];
 char personaje = 'O';
 int personaje_posicion_x = 10;
 int personaje_posicion_y = 5;
-USER_INPUTS input = USER_INPUTS::NONE;
-bool run = true;
 
 
 int main()
 {
 	crearMapa();
 	imprimirMapa();
-	while (run) {
-		inputs();
-		logica();
-		imprimirMapa();
-	}
 }
 
 
@@ -77,74 +67,4 @@ void crearMapa()
 			}
 		}
 	}
-}
-
-
-void inputs()
-{
-	char input_raw;
-	cin >> input_raw;
-	switch (input_raw)
-	{
-	case 'W':
-	case 'w':
-		input = USER_INPUTS::UP;
-		break;
-	case 'A':
-	case 'a':
-		input = USER_INPUTS::LEFT;
-		break;
-	case 'S':
-	case 's':
-		input = USER_INPUTS::DOWN;
-		break;
-	case 'D':
-	case 'd':
-		input = USER_INPUTS::RIGHT;
-		break;
-	case 'Q':
-	case 'q':
-		input = USER_INPUTS::QUIT;
-		break;
-	default:
-		input = USER_INPUTS::NONE;
-		break;
-	}
-
-}
-
-void logica()
-{
-	int personaje_y_new = personaje_posicion_y;
-	int personaje_x_new = personaje_posicion_x;
-
-
-	switch (input)
-	{
-	case NONE:
-		break;
-	case UP:
-		personaje_y_new--;
-		break;
-	case DOWN:
-		personaje_y_new++;
-		break;
-	case RIGHT:
-		personaje_x_new++;
-		break;
-	case LEFT:
-		personaje_x_new--;
-		break;
-	case QUIT:
-		run = false;
-		break;
-	default:
-		break;
-	}
-	if (ConsoleScreen[personaje_y_new][personaje_x_new] == MAP_TILES::WALL) {
-		personaje_y_new = personaje_posicion_y;
-		personaje_x_new = personaje_posicion_x;
-	}
-	personaje_posicion_y = personaje_y_new;
-	personaje_posicion_x = personaje_x_new;
 }
